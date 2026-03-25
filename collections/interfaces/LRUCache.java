df947d284022697f5545240d426771e2cb4b64aa
Azhar Difa Arnanda <aazhard8@gmail.com>
2026-03-25 18:09:46 +0700

    remove class files

--- collections/interfaces/LRUCache.java
+++ collections/interfaces/LRUCache.java
@@ -1,5 +1,16 @@
 package collections.interfaces;
 
+import collections.implementations.Node;
+
 public interface LRUCache {
+
+    boolean put(int key, String val);
+
+    String get(int key);
+
+    boolean remove(int key);
+
+    // Only for testing
+    Node getHead();
 
 }
--- collections/tests/unit/LRUCacheImplTest.java
+++ collections/tests/unit/LRUCacheImplTest.java
@@ -1,5 +1,48 @@
 package collections.tests.unit;
 
+import collections.interfaces.LRUCache;
+import collections.implementations.LRUCacheImpl;
+
 public class LRUCacheImplTest {
+
+    private int         TEST_COUNT = 8;
+
+    public static void main(String[] args) {
+
+        
+
+    }
+
+    private static boolean put_returnTrue_onEmptyCache_andNodeInstertedToHead() {
+        return false;
+    }
+
+    private static boolean put_returnTrue_onFilledCache_andNodeMovedToHead() {
+        return false;
+    }
+
+    private static boolean put_returnTrue_onFullCache_andLeastElemEvicted() {
+        return false;
+    }
+
+    private static boolean get_returnNull_onEmptyCache() {
+        return false;
+    }
+    
+    private static boolean get_returnVal_onFilledCache_andNodeMovedToHead() {
+        return false;
+    }
+
+    private static boolean remove_returnFalse_onEmptyCache() {
+        return false;
+    }
+
+    private static boolean remove_returnFalse_onFilledCache_withNonExistentKey() {
+        return false;
+    }
+
+    private static boolean remove_returnTrue_onFilledCache_andNodeRemoved() {
+        return false;
+    }
     
 }
